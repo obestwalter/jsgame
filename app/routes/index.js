584abb4cbd0cea44b2de598d1737a2cacb938afd
Mark Nijhof <Mark.Nijhof@Gmail.com>
2013-01-24 16:36:51 +0200

    We added a new route

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,6 +11,8 @@ exports.example = function(req, res) {
   var domain    = req.host === 'localhost' ? 'localhost:3000' : req.host.toLowerCase()
 
   var id        = req.params.id
-
+  
+  console.log(id)
+  
   res.render('index', { domain: protocol +'://'+ domain, id: id })
 };
